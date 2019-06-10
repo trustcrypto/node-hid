3feb56f6ac2690178bfa0f213961e698f9bd0d35
onlykey <t@T-2.local>
2019-06-10 17:16:20 -0400

    test

--- src/test-ps3.js
+++ src/test-ps3.js
@@ -2,8 +2,9 @@ var HID = require('../');
 var REPL = require('repl');
 
 var repl = REPL.start('node-hid> ');
-var hid = new HID.HID(1356, 616);
+var hid = new HID.HID(0x16c0,0x0486);
 
+console.log('devices:', HID.devices());
 console.log('features', hid.getFeatureReport(0xf2, 17));
 
 hid.gotData = function (err, data) {
