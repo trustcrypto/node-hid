d38d0dc26850ea9e9a1640ae119e3697174383ad
onlykey <onlykey@users.noreply.github.com>
2019-06-11 11:54:14 -0400

    test

---
+++ src/test.js
@@ -0,0 +1,31 @@
+
+
+var HID = require('../');
+
+var devices = HID.devices();
+
+var isTeensy = function(d) { return d.vendorId===0x16C0 && d.productId===0x0486;}
+var isRawHidUsage = function(d) {
+    return (d.interface===0);
+}
+
+console.log("HID devices:",devices);
+var deviceInfo = devices.find( function(d) {
+  return isTeensy(d) && isRawHidUsage(d);
+});
+console.log("deviceInfo:",deviceInfo.path);
+if( deviceInfo ) {
+    device = new HID.HID(0x16C0,0x0486);
+    console.log("device:",device);
+    device = new HID.HID(deviceInfo.path);
+    console.log("device:",device);
+}
+console.log("device:",device);
+if( !device ) {
+    console.log(devices);
+    console.log("Could not find device in device list");
+
+}
+var feature = device.getFeatureReport(0, 8);
+console.log('get feature report');
+console.log(feature);
